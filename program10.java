a3d7750e01f7abbb70e89cafe3670b50e2b2ff1a
On-cloud7 <priyanka.p62003@gmail.com>
2023-08-16 19:20:59 +0530

    added

---
+++ program10.java
@@ -0,0 +1,9 @@
+public class program10{// for loop
+    public static void main(String[]args){
+        int i;
+        for(i=1;i<=5;i++){
+            System.out.println(" Computer");
+
+        }
+    }
+}
