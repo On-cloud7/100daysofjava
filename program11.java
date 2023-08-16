d932acf13ae08345281b76bd50e48234ef3dd91e
On-cloud7 <priyanka.p62003@gmail.com>
2023-08-16 19:47:08 +0530

    added

---
+++ program11.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class program11 {
+    public static void main(String[]args){
+        int i, n , sum =0;
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter the value of n");
+            n = sc.nextInt();
+        }
+        for(i=1; i<=n ; i++){
+            sum+=i;
+        }
+        System.out.println(sum);
+
+
+
+    }
+    
+}
