fa722dce5c283177aa12a1d138bdc38580646dcf
On-cloud7 <priyanka.p62003@gmail.com>
2023-08-16 21:09:21 +0530

    add

---
+++ program13.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class program13{
+    public static void main(String[]args){
+        int i,n,a,b,c;
+        a=0;
+        b=1;
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter the value of n");
+            n = sc.nextInt();
+            System.out.println("fibonacci:\n0\nl");
+        }
+        for(i=1;i<=n-2;i++){
+            c=a+b;
+            System.out.println(c);
+            a=b;
+            b=c;
+
+        }
+
+    }
+}
