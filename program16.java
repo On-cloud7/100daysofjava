85048bfaa5537deccf50786fc281b8840d272db1
On-cloud7 <priyanka.p62003@gmail.com>
2023-08-19 08:13:40 +0530

    files added and deleted files

---
+++ program16.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class program16{// nested loops
+    public static void main(String[]args){
+        int i,j;
+        for(i=1;i<=5;i++){
+            for(j=1;j<=2;j++){
+                System.out.print("hi! \t");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
