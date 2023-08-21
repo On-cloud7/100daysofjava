cabd0b9c72ce40b7639523c65858d840c52afa80
On-cloud7 <priyanka.p62003@gmail.com>
2023-08-21 22:14:18 +0530

    files added

---
+++ program28.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class program28 {// program to count the nos  of digits in a user entered no
+    public static void main(String[]args){
+        int count=0, n;
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter value of n:");
+        n =sc.nextInt();
+        while (n!=0) {
+         
+            n/= 10;
+            count++;
+                        
+
+                        
+
+                        
+
+            
+        
+        }    
+    
+        System.out.println(count);
+    }
+    
+}
