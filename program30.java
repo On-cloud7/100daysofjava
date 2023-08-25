7400e8846ae12d119a1594b664e5f7d3a5b4a4f6
On-cloud7 <priyanka.p62003@gmail.com>
2023-08-25 06:29:46 +0530

    files added

--- program30.java
+++ program30.java
@@ -1,4 +1,21 @@
-
+import java.util.Scanner;
 public class program30 {
+    public static void main(String[] args) {
+        int i=2,n,sum=0;
+        Scanner sc =new Scanner (System.in);
+        System.out.println(" Enter the value of n");
+        n=sc.nextInt();
+        while (n%i!=0) {
+            i++;
+            
+        }
+        if (n==i) {
+            System.out.println(" prime number");
+            
+        } else {
+            System.out.println(" not a prime number");
+            
+        }
+    }
     
 }
