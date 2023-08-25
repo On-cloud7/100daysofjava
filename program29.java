8863343898bc8a0cc2f086bf3da09fa41a7e7f83
On-cloud7 <priyanka.p62003@gmail.com>
2023-08-25 06:23:19 +0530

    files added

---
+++ program29.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class program29{
+    public static void main(String[] args) {
+        int sum=0,n;
+        Scanner sc =new Scanner(System.in);
+        System.out.println(" Enter value of n");
+        n =sc.nextInt();
+        if (n%10==0) {
+            System.out.println(" Divisible by 10");
+            
+        } else {
+            System.out.println(" Not divisible ");
+            
+        }
+
+    }
+}
