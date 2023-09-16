61e3aba44bbfd5ef69b4995fd116d49b1707012a
On-cloud7 <priyanka.p62003@gmail.com>
2023-09-16 06:53:08 +0530

    files added

---
+++ program35.java
@@ -0,0 +1,12 @@
+public class program35 {
+    public static void main(String[]args){
+        int arr[] = {10,20,11,12,13,14};
+        int sum =0;
+        for(int i=0;i<arr.length;i++){
+            sum =sum + arr[i];
+
+        }
+        System.out.println(sum);
+    }
+    
+}
