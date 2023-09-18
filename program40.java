72760172b1f2181732fba4ae37c279f130261336
On-cloud7 <priyanka.p62003@gmail.com>
2023-09-18 16:35:03 +0530

    files added

---
+++ program40.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class Student{
+    private int id,p,c,m,t;
+    private String name;
+    void accept(){
+        Scanner sc = new Scanner (System.in);
+        String str;
+        System.out.println(" enter name, id and marks in phy, chem, maths");
+        name= sc.nextLine();
+        id= sc.nextInt();
+        p=sc.nextInt();
+        c=sc.nextInt();
+        m=sc.nextInt();
+        t=p+c+m;
+
+    }
+    void display(){
+        System.out.println("Name :"+name+"\nID :"+id+"\nPhy :"+p+"\nchem :"+c+"\nmaths :"+m+"\nTotal :"+t);
+    }
+}
+public class program40{
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.accept();
+        s.display();
+    }
+}
