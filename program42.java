3cfa26e22436c0df96dadc39e8ef31205e6f3e17
On-cloud7 <priyanka.p62003@gmail.com>
2023-09-20 12:26:31 +0530

    files added

--- program42.java
+++ program42.java
@@ -1,3 +1,28 @@
-public   {
+import java.util.Scanner;
+public class program42   {// write a program to find the largest no taken from user
+    public static void main(String[]args){
+        int i,n ,large;
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println(" Enter the no of elements:");
+            n=sc.nextInt();
+            int arr[] =new  int[n];
+            for(i=0;i<arr.length;i++){
+                System.out.println(" enter the no");
+                arr[i]=sc.nextInt();
+            }
+            for(i=0;i<arr.length;i++){
+                System.out.println(arr[i]);
+            }
+            large =arr[0];
+            for(i=0;i<arr.length;i++){
+                if(large<arr[i]);
+                large=arr[i];
+            }
+        }
+        System.out.println(" Largest no "+ large);
+
+
+
+    }
     
 }
