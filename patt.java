24f44301960d78c9af63e355abdd9a1477baeba7
On-cloud7 <priyanka.p62003@gmail.com>
2024-01-02 10:00:48 +0530

    files added

---
+++ patt.java
@@ -0,0 +1,12 @@
+class patt{
+    public static void main(String[] args) {
+        int k =4;
+        int l =5;
+       for(int i =1;i<=k;i++){
+        for(int j =1;j<=l;j++){
+            System.out.print("*");
+        }
+        System.out.println(" ");
+       }
+    }
+}
