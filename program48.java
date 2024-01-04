a4cc3d67cd89499e609a0c0e75cc1f85c8a76f50
On-cloud7 <priyanka.p62003@gmail.com>
2024-01-04 17:38:56 +0530

    file added

---
+++ program48.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class program48 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int arr[][] = new int[3][2];
+        System.out.println(arr.length);
+        for(int row=0; row<arr.length;row++){
+            // for each col in revery row
+            for{int col =0;
+                col++){
+             arr[row][col] = sc.nextInt();
+            }
+        }
+            for(int row=0; row<arr.length;row++){
+                // for each col in revery row
+                for{int col =0;col< arr[row].length;col++){
+                    System.out.println(arr[row][col] + " ");
+                }
+    }
+            }
+        }
+    }
