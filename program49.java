ce02bbce55651795bc6ee662c508da80dbfd79c8
On-cloud7 <priyanka.p62003@gmail.com>
2024-05-29 20:44:11 +0530

    files added

---
+++ program49.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class program49 {
+    public static void main(String[]args){
+    Scanner sc =new Scanner(System.in);
+    int n = sc.nextInt();
+    System.out.println("enter the amount :");
+    if (n == 1){
+        System.out.println(" audi kharedo ");
+    }
+    else  {
+        System.out.println(" scorpeo kharedo ");
+    }
+    System.out.println(" end ");
+}
+}
---
+++ program50.java
@@ -0,0 +1,16 @@
+import java.util.Scanner; 
+public class program50 {
+    public static void main(String[]args){
+        System.out.println(" Enter the number:");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        if (num %2 != 0){
+            System.out.println(" it is a odd number ");
+
+        }
+        else{
+            System.out.println(" it is a even number ");
+        }
+    }
+    
+}
