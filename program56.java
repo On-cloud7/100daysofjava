bcd369e2b22be540a682e97c9b2c61a595261581
On-cloud7 <priyanka.p62003@gmail.com>
2024-05-30 22:36:37 +0530

    files added

---
+++ program56.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class program56 {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner (System.in); 
+        System.out.println(" enter the number:");
+        int num = sc.nextInt();
+        int sum=0;
+        int temp =num;
+        while(num >0){
+            int digit = num%10;
+            sum = sum + (digit*digit*digit);
+            num = num/10;
+        }
+        if(temp ==sum ){
+            System.out.println(" the number is armstrong number");
+        }
+        else {
+            System.out.println(" it is not a amstrong number ");
+        }
+    }
+    
+}
