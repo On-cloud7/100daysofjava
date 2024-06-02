bc9a2f3723aeb3ed2d7c258dfc92e2d2cb1bd065
On-cloud7 <priyanka.p62003@gmail.com>
2024-06-02 13:54:57 +0530

    files added

--- program.java
+++ program.java
@@ -1,5 +1,20 @@
-public class program48{
-    public static void main(String[]args){
-        
+import java.util.Arrays;
+import java.util.Scanner;
+ public class program{
+    public static void main(String[] args) {
+      try (Scanner sc = new Scanner(System.in)) {
+         System.out.println(" enter the no of student:");
+            int student = sc.nextInt();
+            System.out.println(" enter the no of sub:");
+            int sub = sc.nextInt();
+            int [] [] Result = new int [student][sub];
+            for(int row= 0;row<student;row++){
+               System.out.println("enter the marksof student"+(row+1));
+               for(int col =0;col<sub;col++){
+                  Result [row][col] = sc.nextInt();
+               }
+            }
+            System.out.println(Arrays.deepToString(Result));
+      }
     }
-}
+ }
