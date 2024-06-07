428a5e14b725513504da26fe868ecf16d34462cd
On-cloud7 <priyanka.p62003@gmail.com>
2024-06-07 20:24:54 +0530

    files added

---
+++ program59.java
@@ -0,0 +1,12 @@
+public class program59 {
+    public static void main(String[] args) {
+        int n =5;
+        for(int rows =1;rows<=n;rows++){
+            for(int col =1;col<=n ;col++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
